af43ae69a8702617b7b2232ecd5e58c181d44c1b
kenver <79324596+kaan-master@users.noreply.github.com>
2022-07-13 00:02:01 +0200

    Register fix
    Because of some uncertain reason the git didnt save this code.

--- register.php
+++ register.php
@@ -1,21 +1,42 @@
 <?php
-include 'required.php';
+include 'database.php';
 if(isset($_POST['submit']))
 
 {    
 //mysqli_REAL_ESCAPE is om SQL INJECTIONS TE ESCAPEN!
      $username = mysqli_real_escape_string($conn, $_POST['username']);
-    //  $password = mysqli_real_escape_string($conn, $_POST['password']);
-     $password = md5($_POST['password']);
-
+     $password = mysqli_real_escape_string($conn, $_POST['password']);
      $email = mysqli_real_escape_string($conn, $_POST['email']);
+    
+    $sql = "INSERT INTO `users`(`username`, `password`, `email`) 
+     VALUES ('$username', MD5('".$password."'),'$email')";
 
-     
-     $createuser = "INSERT INTO `users`(`username`, `password`, `email`) 
-                          VALUES ('$username','$password','$email')";
-
-     
 }
-if ($conn->query($createuser) === TRUE) {
-  header("location: /sending/index.php");
-  } ?>
+$sql_u = "SELECT * FROM users WHERE username='$username'";
+$sql_e = "SELECT * FROM users WHERE email='$email'";
+$res_u = mysqli_query($conn, $sql_u);
+$res_e = mysqli_query($conn, $sql_e);
+
+
+if (mysqli_num_rows($res_u) > 0) {
+  $takenusername = "Deze account bestaat al.";
+  $home = header("location: /sending/createaccount.php?message=".$takenusername);
+
+ exit();
+} else if(mysqli_num_rows($res_e) > 0){
+  $takenemail = "Dit email adres word al gebruikt";
+  $home = header("location: /sending/createaccount.php?message=".$takenemail);
+}
+else { 
+ 
+  $sql = "INSERT INTO `users`(`username`, `password`, `email`) 
+                       VALUES ('$username', MD5('".$password."'),'$email')";
+  $insert = mysqli_query($conn, $sql);
+  $home = header("location: /sending/index.php");
+
+
+}
+
+?>
+
+?>
