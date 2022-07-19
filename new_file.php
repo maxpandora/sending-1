15df889a9e1520be2c40ec630d1e9fd8344ed042
Kaan Enver Aydin <kaanenveraydin@gmail.com>
2022-07-19 13:26:42 +0200

    2.0
    PDF exports

--- new_file.php
+++ new_file.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php 
+require ('fpdf.php');
+$pdf=new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B', 16);
+$pdf->Cell(80,10,'hello world');
+$pdf->Output('myfile.PDF','I'); 
+?>
