a2def6e135ea1d7504f1edec304f8c6aeb144b48
Kaan Enver Aydin <kaanenveraydin@gmail.com>
2022-08-10 13:28:31 +0200

    update update.php

--- app/update.php
+++ app/update.php
@@ -8,21 +8,27 @@
 </head>
 <body>
   <?php
-include '../database.php';
+include 'database.php';
 if(isset($_POST['submit']))
 
 {    
 
   // mysqli_REAL_ESCAPE is om SQL INJECTIONS TE ESCAPEN!
-     $id = $_GET['id'];
-     $host = mysqli_real_escape_string($conn, $_POST['host']);
-     $ether = mysqli_real_escape_string($conn, $_POST['ether']);
-     $ip = mysqli_real_escape_string($conn, $_POST['ip']);
-     $gateway = mysqli_real_escape_string($conn, $_POST['gateway']);
-     $netmask = mysqli_real_escape_string($conn, $_POST['netmask']);
-     $dns = mysqli_real_escape_string($conn, $_POST['dns']);
-     $netwerk = mysqli_real_escape_string($conn, $_POST['netwerk']);
-     $sql = "UPDATE `pakket` SET `host` = '$host', `ether` = '$ether', `ip` = '$ip', `gateway` = '$gateway', `netmask` = '$netmask', `dns` = '$dns', `netwerk` = '$netwerk' WHERE `pakket`.`id` = $id;";
+     $id       = $_GET['id'];
+     $host     = mysqli_real_escape_string($conn, $_POST['host']);
+     $ether    = mysqli_real_escape_string($conn, $_POST['ether']);
+     $ip       = mysqli_real_escape_string($conn, $_POST['ip']);
+     $gateway  = mysqli_real_escape_string($conn, $_POST['gateway']);
+     $netmask  = mysqli_real_escape_string($conn, $_POST['netmask']);
+     $dns      = mysqli_real_escape_string($conn, $_POST['dns']);
+     $netwerk  = mysqli_real_escape_string($conn, $_POST['netwerk']);
+     $sql = "UPDATE `pakket` SET `host` = '$host',
+                                 `ether` = '$ether',
+                                 `ip` = '$ip',
+                                 `gateway` = '$gateway',
+                                 `netmask` = '$netmask',
+                                 `dns` = '$dns',
+                                 `netwerk` = '$netwerk' WHERE `pakket`.`id` = $id;";
      
 }
 if ($conn->query($sql) === TRUE) {
