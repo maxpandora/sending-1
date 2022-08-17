58877d9e18cf5fac662c318d69250a3af33dae26
Kaan Enver Aydin <kaanenveraydin@gmail.com>
2022-08-17 14:54:36 +0200

    test

--- app/database.php
+++ app/database.php
@@ -3,7 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $database = "sending";
-
+$variable = "test";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
