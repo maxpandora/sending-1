0b3b5c26bb7b65f9896d51679b904a09c70395d1
K. E. Aydın <kaanenveraydin@gmail.com>
2022-10-14 09:23:50 +0200

    Insert Ether with ":"

--- app/insert.php
+++ app/insert.php
@@ -2,10 +2,15 @@
 include 'database.php';
 if(isset($_POST['submit']))
 
+
+
 {    
 //mysqli_REAL_ESCAPE is om SQL INJECTIONS TE ESCAPEN!
      $host =    mysqli_real_escape_string($conn, $_POST['host']);
      $ether =   mysqli_real_escape_string($conn, $_POST['ether']);
+     $end = substr($ether, -12);
+     $chunks = str_split($end, 2);
+     $result = implode(':', $chunks);
      $ip =      mysqli_real_escape_string($conn, $_POST['ip']);
      $gateway = mysqli_real_escape_string($conn, $_POST['gateway']);
      $netmask = mysqli_real_escape_string($conn, $_POST['netmask']);
@@ -13,7 +18,7 @@ if(isset($_POST['submit']))
      $netwerk = mysqli_real_escape_string($conn, $_POST['netwerk']);
 
      $sql = "INSERT INTO `pakket`(`host`, `ip`, `ether`, `gateway`, `netmask`, `dns`, `netwerk`) 
-                          VALUES ('$host','$ip', '$ether','$gateway','$netmask','$dns','$netwerk')";
+                          VALUES ('$host','$ip', '$result','$gateway','$netmask','$dns','$netwerk')";
 
      
 }
