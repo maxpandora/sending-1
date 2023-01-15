6f300280fda50bbc6630c7c5c33e560bea3fd455
K. E. Aydın <kaanenveraydin@gmail.com>
2023-01-15 17:14:40 +0100

    update

--- app/loginscript.php
+++ app/loginscript.php
@@ -17,7 +17,7 @@
     $password = md5($_POST['password']);  
       
         //to prevent from mysqli injection  
-        $username = strtypecslashes($username);  
+        $username = stripcslashes($username);  
         $username = mysqli_real_escape_string($conn, $username);  
         // $password = mysqli_real_escape_string($conn, $password);  
       
